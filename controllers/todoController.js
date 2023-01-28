f8901b217b889306e6ae2f12c48d7dec5ad4d63a
Brogoming <98182104+Brogoming@users.noreply.github.com>
2023-01-28 12:19:27 -0600

    Add files via upload
    setting up the todoController.js and app.js files these first two videos are just setting up thing essentially.

---
+++ controllers/todoController.js
@@ -0,0 +1,17 @@
+//this will control the behavior of our todo list
+module.exports = function(app){
+    //the get request for the url itself
+    app.get('/todo', function(request, response){
+
+    })
+
+    //for when the person adds a new item to the list
+    app.post('/todo', function(request, response){
+
+    })
+
+    //lets the user delete items off the list
+    app.delete('/todo', function(request, response){
+        
+    })
+}
