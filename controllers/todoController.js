1606c754ff599fa4d77d7e237d817560c7b768f8
Brogoming <98182104+Brogoming@users.noreply.github.com>
2023-01-29 13:37:29 -0600

    Add files via upload
    I have successfully created a database and connected it through mongoose with mongodb.

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,23 @@
 //this will control the behavior of our todo list
 
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+
+// connect to the database
+mongoose.connect('mongodb+srv://Test:Test@todo.pskycsu.mongodb.net/todo')
+
+//create a schema - this is like a blue print
+var todoSchema = new mongoose.Schema({
+    item: String
+})
+
+//create a todo model
+var Todo = mongoose.model('Todo', todoSchema) //model and schema
+var itemOne = Todo({ item: 'buy flowers' }).save(function(err){
+    if(err) throw err
+    console.log('item saved')
+})
+
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'program for 7 hours'}] //this array will corespond to the series of items
 var urlencodedParser = bodyParser.urlencoded({extended: false}) //our middleware
 
